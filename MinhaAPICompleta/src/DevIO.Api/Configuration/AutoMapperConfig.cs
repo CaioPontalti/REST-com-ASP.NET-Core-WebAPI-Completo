c9cfc14432088d24327f5c325c15f8c76484a28f
CaioPontalti <43015157+CaioPontalti@users.noreply.github.com>
2020-01-24 16:35:52 -0300

    inicio

---
+++ MinhaAPICompleta/src/DevIO.Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+                           //de => para                
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();//ReverseMap() => Faz o mapeamento inverso 
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
